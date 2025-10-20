6093814f3dc89d92050057aa4a03aa8b3e9a59a3
danythedev01 <dazamar.dev01@gmail.com>
2025-10-20 09:51:42 -0600

    Basic setup for marketing project. Running on localhost 8081

---
+++ packages/marketing/config/webpack.dev.js
@@ -0,0 +1,24 @@
+// merge different webpack configs
+const { merge } = require('webpack-merge');
+
+// take some kind html files and inject a couple of different tags inside of it
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const commonConfig = require('./webpack.common');
+
+const devConfig = {
+    mode: 'development',
+    devServer: {
+        port: 8081,
+        historyApiFallback: {
+            index: 'index.html'
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
+}
+
+module.exports = merge(commonConfig, devConfig);
