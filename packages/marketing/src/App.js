eb2315a378939cd67a2be1a73420f3a7a806bc67
danythedev01 <dazamar.dev01@gmail.com>
2025-10-22 14:10:28 -0600

    add prefix to component classes to avoid css collitions

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core/styles";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 
 import Landing from './components/Landing'
 import Pricing from './components/Pricing'
 
+// adds custom prefix to classes for our microfrontend css to avoid css collitions
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'ma',
+});
+
 export default () => {
     return <div>
-        <StylesProvider>
+        <StylesProvider generateClassName={generateClassName}>
             <BrowserRouter>
                 <Switch>
                     <Route exact path="/pricing" component={Pricing} />
