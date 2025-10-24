2d24b8c22e1afa1d778c067ad67d9c8f5948bed4
danythedev01 <dazamar.dev01@gmail.com>
2025-10-24 16:28:24 -0600

    update gha

--- packages/container/src/components/Header.js
+++ packages/container/src/components/Header.js
@@ -32,7 +32,7 @@ export default function Header({ isSignedIn, onSignOut }) {
             to="/"
             sx={{ textDecoration: 'none' }}
           >
-            App
+            App Test
           </Typography>
           <Button
             color="primary"
